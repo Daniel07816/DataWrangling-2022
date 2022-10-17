447805c22efb80cc84fa777bcff3a7bb135de97d
Daniel Behar Aldana <danielbehar@ufm.edu>
2022-10-16 23:56:13 -0600

    lab terminado, pendiente de limpiar el repo

--- Presentacion_Lab7/Script.R
+++ Presentacion_Lab7/Script.R
@@ -3,9 +3,15 @@ library(highcharter)
 library(lubridate)
 library(corrplot)
 library(ggplot2)
+library(GGally)
+library(Hmisc)
+library(corrplot)
+library(PerformanceAnalytics)
+require(plotly)
 
 data = read.csv("c1.csv")
 
+#---------------Limpieza de data---------------
 data = data[-c(23,24,25,26,27,28)]
 names(data) = tolower(names(data))
 names(data) = gsub("_5", "", names(data))
@@ -36,6 +42,7 @@ data = data %>% mutate(
   factura = gsub("Q", "", factura)
 )
 
+data$fecha = dmy(data$fecha)
 data$factura = as.numeric(data$factura)
 data$camion = as.numeric(data$camion)
 data$pickup = as.numeric(data$pickup)
@@ -49,6 +56,7 @@ data$fijomoto = as.numeric(data$fijomoto)
 data$lat = as.numeric(data$lat)
 data$long = as.numeric(data$long)
 data$height = as.numeric(data$height)
+data$origen = as.factor(data$origen)
 
 data <- data %>%
   mutate(costo_clasificado = (camion + pickup + moto))
@@ -56,30 +64,56 @@ data <- data %>%
 data <- data %>%
   mutate(marge_venta = (factura - (camion + pickup + moto)))
 
-data2 <- data %>% arrange(dmy(data$fecha))
+data2 <- data %>% arrange(data$fecha)
 
-#dataset adicional
-df <- data.frame(data$cod, data$costo_clasificado, data$marge_venta, (data$costo_clasificado - data$marge_venta))
-names(df) = c("cod", "costo", "margen", "diferencia")
+#------------------------------
 
-#Orden de qué actividades son más requeridas
-data %>% group_by(cod) %>% summarise(n = n()) %>% arrange(n) %>% 
-  hchart(type = "column", hcaes(x = cod, y = n))
+highchart(type = "stock") %>% 
+  hc_add_series(data2$factura, type = "line")
 
-#Atividades ordenadas por ingresos (sin costos)
-data %>% group_by(cod) %>% summarise(facturado = sum(factura)) %>% arrange(facturado) %>%
-  hchart(type = "column", hcaes(x = cod, y = facturado))
+highchart(type = "stock") %>% 
+  hc_add_series(data2$costo_clasificado, type = "line")
 
-#Actividades ordenadas por costos
-data %>% group_by(cod) %>% summarise(costeado = sum(costo_clasificado)) %>% arrange(costeado) %>%
-  hchart(type = "column", hcaes(x = cod, y = costeado))
+summary(data)
 
-#Outliers
-boxplot(data$factura)
+#Total marginado hasta el momento
+sum(data$marge_venta)
 
-data2 %>% 
-  ggplot(aes(x=fecha,y=factura)) +
-  labs(title = "Movimiento de las facturas") +
-  xlab("Date") + ylab("Price") +
-  scale_color_manual(values = "Black")+
-  geom_line()
+#Tarifas promedio del 2017 por unidad
+camiones = data.frame(data$camion,data$factura)
+camiones = camiones %>% mutate(data.camion = na_if(camiones$data.camion,0))
+camiones = camiones[!is.na(camiones$data.camion),]
+
+pickups = data.frame(data$pickup,data$factura)
+pickups = pickups %>% mutate(data.pickup = na_if(pickups$data.pickup,0))
+pickups = pickups[!is.na(pickups$data.pickup),]
+
+motos = data.frame(data$moto,data$factura)
+motos = motos %>% mutate(data.moto = na_if(motos$data.moto,0))
+motos = motos[!is.na(motos$data.moto),]
+
+summary(camiones)
+summary(pickups)
+summary(motos)
+
+#Codigo por cada centro
+ubicaiones = data %>%
+  group_by(cod,origen) %>%
+  summarise(Origen = n())
+
+ggplot(ubicaiones, aes(x = origen, y = Origen, fill = cod))+geom_bar(
+  stat = "identity") + geom_text(aes(label = Origen), position = 
+  position_stack(vjust = 0.4), size = 3)
+
+#Mejores facturas por sector
+facturas = data %>%
+  select(factura, cod) %>% 
+  group_by(cod) %>% 
+  summarise(cantidad = n()) %>% 
+  arrange(desc(cantidad))
+
+marginacion = (data$marge_venta/data$factura)
+mean(marginacion)
+
+ggplot(facturas, aes(x = cod, y = cantidad)) + geom_bar(color = "red",
+  fill = "red", stat = "identity")
